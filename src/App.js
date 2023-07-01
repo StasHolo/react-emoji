b446753806e106aee832ddd0b5cc369d3effce64
Stas <Stass1994@yandex.ru>
2023-07-01 13:04:46 +0500

    fix functions

--- src/App.js
+++ src/App.js
@@ -1,33 +1,24 @@
 import { useState } from 'react';
 import './App.css';
-  let i=0
-  let k=1
-
-  let a=0
-  let b=1
-
-  let c=0
-  let d=1
-
 
 function App() {
 
-  const points1 = useState(0)
-  const points2 = useState(0)
-  const points3 = useState(0)
+  const [points1, setPoints1] = useState(0)
+  const [points2, setPoints2] = useState(0)
+  const [points3, setPoints3] = useState(0)
+
+  const i = 1
+  
   
 
   function clickImg1(){
-    i=i+k
-    points1[1](i)
+    setPoints1(points1+i)
     }
   function clickImg2(){
-    a=a+b
-    points2[1](a)
+    setPoints2(points2+i)
     }
   function clickImg3(){
-    c=c+d
-    points3[1](c)
+    setPoints3(points3+i)
     }
   
     
@@ -41,15 +32,15 @@ function App() {
        <div className='points-block'>
         <div className='points'>
         <img className='image-class' id='img-cookie' src='em1.png' onClick={clickImg1}></img>
-          {points1[0]}
+          {points1}
           </div>
         <div className='points'>
         <img className='image-class' id='img-cookie' src='em2.png' onClick={clickImg2}></img>
-          {points2[0]}
+          {points2}
           </div>
         <div className='points'>
         <img className='image-class' id='img-cookie' src='em3.png' onClick={clickImg3}></img>
-        {points3[0]}
+        {points3}
         </div>
         </div>
     </div>
